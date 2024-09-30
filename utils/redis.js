4bb48a75627660500712e9496bb23ae3a58edb6f
AnasMouak <anasmouak1111@gmail.com>
2024-09-30 07:09:25 -0700

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,57 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (err) => {
+      console.error(`Redis client error: ${err}`);
+    });
+
+    // Promisify the Redis functions to use them with async/await
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+
+    this.connected = false;
+
+    // Listen for the 'connect' event to confirm connection
+    this.client.on('connect', () => {
+      this.connected = true;
+    });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async get(key) {
+    try {
+      const value = await this.getAsync(key);
+      return value;
+    } catch (error) {
+      console.error(`Error getting key ${key}: ${error}`);
+      return null;
+    }
+  }
+
+  async set(key, value, duration) {
+    try {
+      await this.setAsync(key, value, 'EX', duration);
+    } catch (error) {
+      console.error(`Error setting key ${key}: ${error}`);
+    }
+  }
+
+  async del(key) {
+    try {
+      await this.delAsync(key);
+    } catch (error) {
+      console.error(`Error deleting key ${key}: ${error}`);
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
