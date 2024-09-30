0c84640d58f08d1716d92d70498d960d076ef7af
AnasMouak <anasmouak1111@gmail.com>
2024-09-30 12:47:45 -0700

    Image Thumbnails

---
+++ worker.js
@@ -0,0 +1,74 @@
+import Queue from 'bull';
+import { ObjectId } from 'mongodb';
+import imageThumbnail from 'image-thumbnail';
+import fs from 'fs';
+import dbClient from './utils/db';
+
+const fileQueue = new Queue('fileQueue', {
+  redis: {
+    host: '127.0.0.1',
+    port: 6379,
+  },
+});
+
+const userQueue = new Queue('userQueue', {
+  redis: {
+    host: '127.0.0.1',
+    port: 6379,
+  },
+});
+
+async function generateThumbnail(filePath, width) {
+  try {
+    const thumbnail = await imageThumbnail(filePath, { width });
+    const thumbnailPath = `${filePath}_${width}`;
+    await fs.promises.writeFile(thumbnailPath, thumbnail);
+  } catch (error) {
+    console.error(`Error generating thumbnail: ${error}`);
+  }
+}
+
+fileQueue.process(async (job) => {
+  const { userId, fileId } = job.data;
+
+  if (!fileId) {
+    throw new Error('Missing fileId');
+  }
+
+  if (!userId) {
+    throw new Error('Missing userId');
+  }
+
+  const file = await dbClient.db.collection('files').findOne({
+    _id: ObjectId(fileId),
+    userId: ObjectId(userId),
+  });
+
+  if (!file) {
+    throw new Error('File not found');
+  }
+
+  const filePath = file.localPath;
+
+  await generateThumbnail(filePath, 500);
+  await generateThumbnail(filePath, 250);
+  await generateThumbnail(filePath, 100);
+});
+
+userQueue.process(async (job) => {
+    const { userId } = job.data;
+  
+    if (!userId) {
+      throw new Error('Missing userId');
+    }
+  
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+  
+    if (!user) {
+      throw new Error('User not found');
+    }
+  
+    console.log(`Welcome ${user.email}!`);
+});
+  
+console.log('Worker is running');
